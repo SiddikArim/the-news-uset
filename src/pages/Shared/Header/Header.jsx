9dd84947429928b9e3510072226e10ead9f60c26
Siddik Arim <arim99999@gmail.com>
2024-05-27 09:20:33 +0600

    vid-4

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,11 @@
+import moment from "moment/moment";
 import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h2>this is header</h2>
+    <div className="text-center">
+      <h2>The USET NEWS</h2>
+      <p>{moment().format("llll")}</p>
     </div>
   );
 };
