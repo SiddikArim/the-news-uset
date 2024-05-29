c60f419f7d3d06cd6efff7e6457697eb25909d9d
Siddik Arim <arim99999@gmail.com>
2024-05-29 16:26:44 +0600

    sending dwn

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,12 +1,42 @@
 import moment from "moment/moment";
 import React from "react";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
-    <div className="text-center">
-      <h2>The USET NEWS</h2>
-      <p>{moment().format("llll")}</p>
-    </div>
+    <Container>
+      <div className="text-center">
+        <h2>The USET NEWS</h2>
+        <p>{moment().format("llll")}</p>
+      </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="text-danger">
+          প্রতিশ্রুতি অনুযায়ী উদ্বোধনী সেমিস্টারের শিক্ষার্থীদের হাতে ল্যাপটপ
+          তুলে দিয়েছে ইউনিভার্সিটি অব স্কিল এনরিচমেন্ট অ্যান্ড টেকনোলজি (ইউসেট)
+          কর্তৃপক্ষ।
+        </Marquee>
+      </div>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#features">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button variant="secondary">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 };
 
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
 const RightNav = () => {
   return (
     <div>
-      <h2>right nav</h2>
+      <h4>Login with</h4>
+      <Button className="mb-2" variant="outline-primary">
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub /> Login with Github
+      </Button>
+      <div>
+        <h4 className="mb-2 mt-2">Find us on</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook />
+            Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter />
+            Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram />
+            Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
     </div>
   );
 };
