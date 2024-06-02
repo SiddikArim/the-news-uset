c9efa47028ef607af8a30fde6ab390d0a0959dd0
SiddikArim <arim99999@gmail.com>
2024-06-02 13:14:27 +0600

    sending

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -6,16 +6,13 @@ import LeftNav from "../pages/Shared/leftNav/LeftNav";
 import RightNav from "../pages/Shared/RightNav/RightNav";
 import { Outlet } from "react-router-dom";
 
-const Main = () => {
+const NewsLayout = () => {
   return (
     <div>
       <Header></Header>
       <Container>
         <Row>
-          <Col lg={3}>
-            <LeftNav />
-          </Col>
-          <Col lg={6}>
+          <Col lg={9}>
             <Outlet />
           </Col>
           <Col lg={3}>
@@ -28,4 +25,4 @@ const Main = () => {
   );
 };
 
-export default Main;
+export default NewsLayout;
